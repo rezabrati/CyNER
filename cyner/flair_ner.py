f00dc1e728b847c21ff0a101c11b209cbf7acb07
Reza Barati <118802332+rezabrati@users.noreply.github.com>
2024-06-10 13:50:12 +0200

    Update flair_ner.py

--- cyner/flair_ner.py
+++ cyner/flair_ner.py
@@ -23,5 +23,6 @@ class Flair(EntityExtraction):
         entities = []
         for x in pred['entities']:
             # 'labels' are formatted as [(TAG prob), ...]
-                entities.append(Entity(x['start_pos'], x['end_pos'], x['text'], x['labels'][0], x['labels'][0]))
+                # entities.append(Entity(x['start_pos'], x['end_pos'], x['text'], x['labels'][0], x['labels'][0]))
+                entities.append(Entity(x['start_pos'], x['end_pos'], x['text'], x['labels'][0]))
         return entities
